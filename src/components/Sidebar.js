12557eba36ced0fb1b33253a581bd6deb2d9c270
nelleeeee <rudghksldl@gmail.com>
2020-09-23 01:23:04 +0900

    finished frontend

---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  const addNewChat = true;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat={addNewChat} />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsU7h3HZ5k7b8fOPIxGuPkpQ9PTstCWIQ",
+  authDomain: "whats-clone-ba472.firebaseapp.com",
+  databaseURL: "https://whats-clone-ba472.firebaseio.com",
+  projectId: "whats-clone-ba472",
+  storageBucket: "whats-clone-ba472.appspot.com",
+  messagingSenderId: "915401381360",
+  appId: "1:915401381360:web:323e42335669780866b935",
+  measurementId: "G-4LDY84DPRQ",
+};
